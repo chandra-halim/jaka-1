db44bed3120216e6febe12ab81ab765ed62ed84a
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-10-13 02:11:23 +0700

    Update login.py

--- login.py
+++ login.py
@@ -1,5 +1,5 @@
+# login.py
 import streamlit as st
-# import streamlit.components.v1 as components
 
 user_agreement = """
     1. User perlu memasukkan username dan password SSO sebagai persyaratan login pada SIAK-NG
@@ -8,14 +8,21 @@ user_agreement = """
     4. Dengan mencentang "Saya setuju" pada akhir halaman ini berarti User telah menyetujui seluruh poin di atas.
     """
 
+user_login = {
+    "username"  : "",
+    "password"  : "",
+    "agreement" : False,
+    "login"     : False,
+}
+
 def app():
     st.subheader('User Agreement')
     st.markdown(user_agreement, unsafe_allow_html=True)
-    st.info('Harap dibaca secara saksama User Agreement di atas sebelum menyetujuinya')
-    username = st.sidebar.text_input('Username')
-    password = st.sidebar.text_input('Password', type='password')
-    agree = st.sidebar.checkbox('Saya telah membaca dan menyetujui User Agreement di samping.')
-    if agree:
-        return st.sidebar.button('Login')
-    else:
-        return False
+    st.info('Harap dibaca secara saksama User Agreement di atas sebelum menyetujuinya.')
+    
+    user_login['username']  = st.sidebar.text_input('Username', max_chars=20)
+    user_login['password']  = st.sidebar.text_input('Password', type='password')
+    user_login['agree'] = st.sidebar.checkbox('Saya telah membaca dan menyetujui User Agreement')
+    user_login["login"] = st.sidebar.button('Login')
+    
+    return user_login
