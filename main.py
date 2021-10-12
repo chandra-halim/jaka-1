fd06e565e4fceb7780dd3502c4b2bdbead9d7cec
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-10-13 00:49:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# app.py
+# main.py
 import home
 import schedule
 import login
@@ -9,6 +9,17 @@ PAGES = {
     "Schedule": schedule,
     "Login SSO": login
 }
+
+st.set_page_config(page_title='JAKA', page_icon=":books:")
+
+hide_menu_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 st.sidebar.title('Navigation')
 selected = st.sidebar.selectbox('Go To', PAGES.keys())
