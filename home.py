1dd67e463a2db2cbca848c2a19b538abda19d3e5
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-10-15 17:05:16 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,12 @@
 # app1.py
 import streamlit as st
 def app():
+    p1 = '''
+        <p style="color:blue;font-size:46px;">
+            I'm a big, blue, <strong>strong</strong> paragraph
+        </p>
+        '''
     st.title('APP1')
     st.color_picker('Pick A Cplor', '#00f900')
+    st.markdown(p1, unsafe_allow_html=True)
     st.write('Welcome to app1')
