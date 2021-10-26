b5edd1184caaf8a628251be4e0ea580b69857286
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-10-27 04:27:55 +0700

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -31,28 +31,28 @@ def app():
         for i in range(2):
             body.write(' ')
             
-        st.markdown('''
-            <style>
-            .button {
-              background-color: #f72585;
-              border: none;
-              color: white;
-              padding: 15px 32px;
-              text-align: center;
-              text-decoration: none;
-              display: inline-block;
-              font-size: 16px;
-              margin: 4px 2px;
-              cursor: pointer;
-              width: 50%;
-            }
+#         st.markdown('''
+#             <style>
+#             .button {
+#               background-color: #f72585;
+#               border: none;
+#               color: white;
+#               padding: 15px 32px;
+#               text-align: center;
+#               text-decoration: none;
+#               display: inline-block;
+#               font-size: 16px;
+#               margin: 4px 2px;
+#               cursor: pointer;
+#               width: 50%;
+#             }
 
-            .button:hover {
-              box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
-            }
-            </style>
-            <button type="button" onclick="alert('Hello world!')"> Get Started </button>
-        ''', unsafe_allow_html=True)
+#             .button:hover {
+#               box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+#             }
+#             </style>
+#             <button type="button" onclick="alert('Hello world!')"> Get Started </button>
+#         ''', unsafe_allow_html=True)
         started = st.button("Get Started", help='klik untuk memulai!')
         if started:
             st.balloons()
