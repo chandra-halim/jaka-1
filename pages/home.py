e16d2f97af44d4cbd72a85a159f37fcbd73ea793
Chandra Halim Nuruddin <39824546+chalimmm@users.noreply.github.com>
2021-10-27 15:33:49 +0700

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -2,6 +2,7 @@ import streamlit as st
 import json
 import requests
 from streamlit_lottie import st_lottie
+from pages import error
 
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
@@ -12,47 +13,10 @@ def load_lottieurl(url: str):
     if r.status_code != 200:
         return None
     return r.json()
-    
+
 def app():
     c1, c2 = st.columns(2)
     
-    with c1:
-        body = st.container()
-        for i in range(8):
-            body.write(' ')
-        body.title("JAKA", 'get-started')
-        about_text = """
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        """
-        body.write(about_text)
-        for i in range(2):
-            body.write(' ')
-            
-        st.markdown("""
-            <style>
-            div.stButton > button:first-child {
-                background-color: #f72585;
-                border: none;
-                color: white;
-                padding: 15px 32px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 16px;
-                margin: 4px 2px;
-                cursor: pointer;
-                width: 40%;
-            }
-            </style>""", unsafe_allow_html=True)
-        
-        started = st.button("Get Started")
-        
-        if started:
-            st.balloons()
-            st.success('Cieee, udah siap nih buat mulai')
     with c2:
         lottie_file = load_lottiefile("assets/schedule.json")  # replace link to local lottie file
         # lottie_file = load_lottieurl("https://assets2.lottiefiles.com/packages/lf20_ok9cq9zj.json")
@@ -68,3 +32,50 @@ def app():
             width=None,
             key="lottie-schedule",
         )
+    
+    with c1:
+        if 'start' not in st.session_state:
+            st.session_state['start'] = False
+        
+        body = st.container()
+        
+        for i in range(2):
+            body.write(' ')
+        
+        st.warning(st.session_state['start'])
+        
+        st.markdown("""
+        <style>
+        div.stButton > button {
+            background-color: #f72585;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            width: 50%;
+        }
+        div.stButton > button:hover {
+            background-color: #f8f8f8;
+            box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 10px 20px 0 rgba(0,0,0,0.19);
+        }
+        </style>""", unsafe_allow_html=True)
+        
+        if st.button("Get Started") or st.session_state['start']:
+            st.session_state['start'] = True
+            body.success('Cieee udah ready nih')
+        else:
+            body.title("JAKA", 'get-started')
+            about_text = """
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            """
+            body.write(about_text)
+            for i in range(2):
+                body.write(' ')
