b2d6865794d4efecea6d5cf4eb7890e40f79f967
alvinzahran <78026758+alvinzahran@users.noreply.github.com>
2021-11-01 15:10:59 +0700

    Update index.py

--- index.py
+++ index.py
@@ -26,9 +26,10 @@ h1, h2 = st.columns((5, 1))
 
 with h1: # Kolom kiri untuk logo
     st.write(" ")
+    st.write(" ")
     st.image(
         'https://i.ibb.co/yP2wjhW/jaka-02.png',
-        width=100
+        width=80
     )
 
 with h2: # Dropdown menu
