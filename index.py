4fd6515e7efe96e782883851f5bf9f947f7a7f00
fdsyawalino <90434721+fdsyawalino@users.noreply.github.com>
2021-11-08 15:16:33 +0700

    Update index.py

--- index.py
+++ index.py
@@ -29,18 +29,11 @@ st.markdown("""
   <li class="nav-item">
     <a class="nav-link active" href="#">Home</a>
   </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">Login</a>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Action</a>
-      <a class="dropdown-item" href="#">Another action</a>
-      <a class="dropdown-item" href="#">Something else here</a>
-      <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="#">Separated link</a>
-    </div>
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Login</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" href="#">Schedule</a>
   </li>
   <li class="nav-item">
     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
